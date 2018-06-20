ead046518e3db5912feeb5566a8d33032ce348b3
chrisaor <pchpch0070@gmail.com>
2018-06-20 15:02:27 +0900

    relation 구현

--- app/blog/models/user_info.py
+++ app/blog/models/user_info.py
@@ -12,7 +12,8 @@ class BlogUser(models.Model):
     )
     block_users = models.ManyToManyField(
         'self',
-        symmetrical=False
+        symmetrical=False,
+        through='Relation',
     )
 
     def __str__(self):
@@ -29,3 +30,32 @@ class UserInfo(models.Model):
 
     def __str__(self):
         return f'{self.user}님의 정보'
+
+class Relation(models.Model):
+    CHOICE_RELATION_TYPE = (
+        ('f', 'Follow'),
+        ('b', 'Block'),
+    )
+    from_user = models.ForeignKey(
+        BlogUser,
+        on_delete=models.CASCADE,
+        related_name='relation_by_from_user',
+    )
+
+    to_user = models.ForeignKey(
+        BlogUser,
+        on_delete=models.CASCADE,
+        related_name='relation_by_to_user',
+    )
+
+    relation_type = models.CharField(
+        max_length=1,
+        choices=CHOICE_RELATION_TYPE,
+    )
+
+    def __str__(self):
+        return 'from({}), to({}), {}'.format(
+            self.from_user.name,
+            self.to_user.name,
+            self.get_relation_type_display(),
+        )
